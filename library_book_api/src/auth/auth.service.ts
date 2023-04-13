3a9bd89a585947ac6e755fdb216936a8f3a5dc3a
yadlapalli9999 <yadlapalli9999>
2023-04-13 12:10:23 +0530

    signup and login and jwt token

---
+++ library_book_api/src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable,UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(@InjectModel(User.name)
+    private userModel:Model<User>,
+    private jwtService:JwtService
+    ){}
+
+    async signUp(signUpDto:SignUpDto):Promise<{token:string}>{
+        let {name,email,password} = signUpDto;
+        const hashPassword = await bcrypt.hash(password,10);
+
+        const user = await this.userModel.create({name,email,password:hashPassword});
+
+        const token = this.jwtService.sign({id:user._id})
+
+        return {token}
+    }
+
+    async login(loginDto:LoginDto):Promise<{token:string}>{
+        let {email,password} = loginDto;
+        const user = await this.userModel.findOne({email})
+
+        if(!user){
+            throw new UnauthorizedException("Invaild email or password")
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password,user.password)
+        if(!isPasswordMatched){
+            throw new UnauthorizedException("Invaild email or password")
+        }
+
+        const token = this.jwtService.sign({id:user._id})
+
+        return {token}
+    }
+}
